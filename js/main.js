e74045f1dc6a13b39494568993f5b8d4188bffef
RyazanovAndrey <ryazanov.andrej@gmail.com>
2023-05-30 13:04:32 +0300

    13.04

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const links = document.querySelectorAll('.menu__link');
 const drop = document.querySelector('.drop');
 
 
-links.forEach(item =>{
-    item.addEventListener('click', (event)=> {
+links.forEach(item => {
+    item.addEventListener('click', (event) => {
 
         let links = event.target;
         let drop = links.closest('.menu__item').querySelector('.drop');
-        if(drop){
+        if (drop) {
             drop.classList.toggle('open');
             links.classList.toggle('arrow-rotate');
         }
@@ -15,12 +15,41 @@ links.forEach(item =>{
 });
 
 
-document.addEventListener('click', (event)=> {
-    if(!event.target.closest('.menu')){
+document.addEventListener('click', (event) => {
+    if (!event.target.closest('.menu')) {
         drop.classList.remove('open');
         links.forEach(item => item.classList.remove('arrow-rotate'));
     }
 });
+
+// dark-toggle
+
+const btnTheme = document.querySelector('.header__btn-theme');
+const theme = document.querySelector('.theme');
+const themeIcon = document.querySelector('.theme-icon');
+
+let toggleTheme = false;
+
+if(localStorage.getItem('theme') == 'true'){
+    theme.href = 'css/dark.css';
+    themeIcon.src = 'img/sun-icon.svg';
+    localStorage.setItem('theme', true);
+}
+
+btnTheme.addEventListener('click', () => {
+
+    if (toggleTheme == false) {
+        theme.href = 'css/dark.css';
+        themeIcon.src = 'img/sun-icon.svg';
+        localStorage.setItem('theme', true);
+        toggleTheme = !toggleTheme;
+    }else{
+        theme.href = 'css/main.css';
+        themeIcon.src = 'img/moon-icon.svg';
+        localStorage.setItem('theme', false);
+        toggleTheme = !toggleTheme;
+    }
+})
 
 
 // phone-mask
@@ -28,8 +57,8 @@ document.addEventListener('click', (event)=> {
 mask('[data-tel]');
 const dataTel = document.querySelector('[data-tel]');
 
-dataTel.addEventListener('blur', ()=> {
-    if(dataTel.value == '+'){
+dataTel.addEventListener('blur', () => {
+    if (dataTel.value == '+') {
         dataTel.value = ''
     }
 })
@@ -39,7 +68,7 @@ dataTel.addEventListener('blur', ()=> {
 const burger = document.querySelector('.burger');
 const menu = document.querySelector('.menu');
 
-burger.addEventListener('click', ()=> {
+burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
 })
